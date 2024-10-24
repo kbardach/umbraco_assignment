b0909c5349a67c598f189dfadecfc728eabd8886
Lara Pirdaoud <lara.pirdaoud@hotmail.com>
2024-10-24 17:37:42 +0200

    Added error page

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.Controllers;
+using umbraco_assignment.Models.PublishedModels;
+using umbraco_assignment.Models.ViewModels;
+
+namespace umbraco_assignment.Controllers
+{
+	public class ErrorController : RenderController
+	{
+		private readonly IUmbracoContextAccessor _umbracoContextAccessor;
+
+		public ErrorController(ILogger<RenderController> logger, ICompositeViewEngine compositeViewEngine, IUmbracoContextAccessor umbracoContextAccessor) : base(logger, compositeViewEngine, umbracoContextAccessor)
+		{
+			_umbracoContextAccessor = umbracoContextAccessor;
+		}
+
+		public override IActionResult Index()
+		{
+			var errorPage = CurrentPage as Error;
+
+			if (errorPage != null)
+			{
+				var model = new ErrorPageViewModel(errorPage, _umbracoContextAccessor);
+
+				return CurrentTemplate(model);
+			}
+
+			return null;
+		}
+	}
+}
