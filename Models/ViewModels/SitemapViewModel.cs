7b6b71cda74908a70817be86083d9e3fd6915e7b
Erikolssonr <Erikolssonr@gmail.com>
2024-10-25 15:06:35 +0200

    Start of sitemap

---
+++ Models/ViewModels/SitemapViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.Web;
+using umbraco_assignment.Models.PublishedModels;
+using umbraco_assignment.Models.ViewModels;
+
+namespace Nackademin_Umbraco_001.Models.ViewModels
+{
+    public class SitemapViewModel : BasePageViewModel<Sitemap>
+    {
+        public List<IPublishedContent> Pages { get; set; }
+
+        public SitemapViewModel(Sitemap content, IUmbracoContextAccessor umbracoContextAccessor) : base(content, umbracoContextAccessor)
+        {
+        }
+    }
+}
