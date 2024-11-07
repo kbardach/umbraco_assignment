f073b25376644a19ba14ba77e8c77536c1dbafd7
kbardach <144119408+kbardach@users.noreply.github.com>
2024-11-07 17:41:42 +0100

    added forms to umbraco backoffice+view

---
+++ Models/ViewModels/ContactUsPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Umbraco.Cms.Core.Web;
+using umbraco_assignment.Models.PublishedModels;
+
+namespace umbraco_assignment.Models.ViewModels
+{
+	public class ContactUsPageViewModel : BasePageViewModel<ContactUs>
+	{
+		public ContactUsPageViewModel(ContactUs content, IUmbracoContextAccessor umbracoContextAccessor) : base(content, umbracoContextAccessor)
+		{
+		}
+	}
+}
