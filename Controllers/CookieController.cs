11ff68b2c0cf65408e11ee408e148c97bf756718
Lara Pirdaoud <lara.pirdaoud@hotmail.com>
2024-11-12 15:03:44 +0100

    updated cookies

---
+++ Controllers/CookieController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.Controllers;
+using umbraco_assignment.Models.ViewModels;
+using umbraco_assignment.Models.PublishedModels;
+
+namespace umbraco_assignment.Controllers
+{
+    public class CookieController : RenderController
+    {
+        private readonly IUmbracoContextAccessor _umbracoContextAccessor;
+
+        public CookieController(ILogger<RenderController> logger, ICompositeViewEngine compositeViewEngine, IUmbracoContextAccessor umbracoContextAccessor) : base(logger, compositeViewEngine, umbracoContextAccessor)
+        {
+            _umbracoContextAccessor = umbracoContextAccessor;
+        }
+
+        public override IActionResult Index()
+        {
+            var cookiePage = CurrentPage as CookieDeclaration;
+
+            if (cookiePage != null)
+            {
+                var model = new CookiePageViewModel(cookiePage, _umbracoContextAccessor);
+
+                return CurrentTemplate(model);
+            }
+
+            return null;
+        }
+    }
+}
