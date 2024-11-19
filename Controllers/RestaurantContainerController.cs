867f4e574ed010e5b97d86d921dcf4cbe26756d8
kbardach <144119408+kbardach@users.noreply.github.com>
2024-11-19 13:34:51 +0100

    added restaurantController for redirect to search

---
+++ Controllers/RestaurantContainerController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.Extensions.Logging;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.Controllers;
+
+namespace umbraco_assignment.Controllers
+{
+    public class RestaurantContainerController : RenderController
+    {
+        public RestaurantContainerController(
+            ILogger<RestaurantContainerController> logger,
+            ICompositeViewEngine compositeViewEngine,
+            IUmbracoContextAccessor umbracoContextAccessor)
+            : base(logger, compositeViewEngine, umbracoContextAccessor)
+        { }
+
+        public override IActionResult Index()
+        {
+            var currentCulture = System.Globalization.CultureInfo.CurrentUICulture.Name;
+            string redirectUrl = currentCulture == "sv" ? "/sok" : "/en/search";
+            return Redirect(redirectUrl);
+        }
+    }
+}
